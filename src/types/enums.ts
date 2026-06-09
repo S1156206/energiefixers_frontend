887ba108d2a5e6940212f62d79effcbd2e27c22c
Invictum-Fitness <info@invictum-fitness.com>
2026-06-09 16:21:40 +0200

    feat: made enums file with used enums

---
+++ src/types/enums.ts
@@ -0,0 +1,38 @@
+export enum EmailStatus {
+  NO_EMAIL = 'NO_EMAIL',
+  OPT_OUT = 'OPT_OUT',
+  DELIVERABLE = 'DELIVERABLE',
+}
+
+export enum EnergyLabel {
+  A_PLUS_PLUS_PLUS,
+  A_PLUS_PLUS,
+  A_PLUS,
+  A,
+  B,
+  C,
+  D,
+  E,
+  F,
+  G,
+}
+
+export enum InvitationType {
+  REGISTRATION = 'REGISTRATION',
+  ANNUAL_REMINDER = 'ANNUAL_REMINDER',
+}
+
+export enum InvitationStatus {
+  PENDING = 'PENDING',
+  ACCEPTED = 'ACCEPTED',
+  EXPIRED = 'EXPIRED',
+  REVOKED = 'REVOKED',
+}
+
+export enum Category {
+  INSULATION,
+  LIGHTING,
+  WATER,
+  VENTILATION,
+  OTHER,
+}
